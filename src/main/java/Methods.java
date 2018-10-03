68a28db7b4dd52e7304b9e12b1fcbcbb87b0d4d3
artemsalita <salita2212@gmail.com>
2018-10-03 20:58:01 +0600

    M P

---
+++ src/main/java/Methods.java
@@ -0,0 +1,17 @@
+public class Methods {
+    //В классе Methods реализуйте публичный статический метод task1860.
+   public static void task1860(String x){//Он принимает в качестве аргумента 1 строку.
+
+      System.out.println("Мы стремимся к "+x);
+   }
+
+    public static void task4140(String firstName,String lastName) {
+        System.out.println("Меня зовут "+lastName+", "+firstName+" "+lastName);
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/Program.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args) {
+        //В методе main класса Program 3 раза вызовите метод task1860 класса Methods.
+//        Methods.task1860("знаниям");
+//        Methods.task1860("цели");
+//        Methods.task1860("результату");
+
+//        Methods.task4140("Джеймс","Бонд");
+//        Methods.task4140("капитан","Джек Воробей");
+//        Methods.task4140("Александр","Крюков");
+
+    }
+}
