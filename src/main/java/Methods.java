ed7f95b83f5f2c556a5f3d834b3dabe63068867e
Artem <salita2212@gmail.com>
2018-10-05 18:57:45 +0600

    3.4

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -7,9 +7,16 @@ public class Methods {
 
     public static void task4140(String firstName,String lastName) {
         System.out.println("Меня зовут "+lastName+", "+firstName+" "+lastName);
+    }
 
+    public static void task2632(String cx, String cy, String r, String fill) {
+        System.out.println("<"+"circle cx="+"\""+cx+"\""+" "+"cy="+"\""+cy+"\""+" "+"r="+"\""+r+"\""+" "+"fill="+"\""+fill+"\""+"/"+">");
 
+    }
 
+    public static void task7474(String a, String b, String c) {
+        System.out.println("Квадратное уравнение "+a+"*x*x + "+b+"*x + "+c+" = 0.\n" +
+                           "Его дискриминант вычисляется по формуле: d = "+b+"*"+b+" - 4*"+a+"*"+c+"");
     }
 
 
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,13 +1,26 @@
 public class Program {
     public static void main(String[] args) {
-        //В методе main класса Program 3 раза вызовите метод task1860 класса Methods.
+       // В методе main класса Program 3 раза вызовите метод task1860 класса Methods.
 //        Methods.task1860("знаниям");
 //        Methods.task1860("цели");
 //        Methods.task1860("результату");
-
+//
 //        Methods.task4140("Джеймс","Бонд");
 //        Methods.task4140("капитан","Джек Воробей");
 //        Methods.task4140("Александр","Крюков");
+//
+//        Methods.task2632("50","50","40","yellow");
+//        Methods.task2632("400","300","200","green");
+//        Methods.task2632("254","356","100","#cc6600");
+
+        Methods.task7474("1","2","3");
+        System.out.println();
+        Methods.task7474("-3","0","2");
+
+
+
+
+
 
     }
 }
