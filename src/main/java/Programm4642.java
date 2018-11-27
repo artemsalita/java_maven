4ded4ad6da6b587c2f9a2294f53450dd14561542
Artem <salita2212@gmail.com>
2018-11-28 00:05:27 +0600

    4642

---
+++ src/main/java/Programm4642.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Programm4642 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String A = s.nextLine();
+        String filename = "resourses/task4642/test"+ A +".txt";
+        File file = new File(filename);
+        ArrayList<String> lines = new ArrayList<String>();
+
+        try {
+            Scanner d = new Scanner(file);
+
+            String line = d.nextLine();
+            //System.out.println(line);
+
+            while (d.hasNext()){
+                line = d.nextLine();
+                lines.add(line);
+                //System.out.print(", " + line);
+            }
+
+        } catch (java.io.FileNotFoundException ex) {
+            System.out.println("Файл не существует " + file.getAbsolutePath());
+        }
+        catch (java.util.NoSuchElementException exx){
+            System.out.println("Файл пуст " + file.getAbsolutePath());
+        }
+
+        System.out.print(lines.size());
+
+
+
+
+    }
+
+}
