f292db4c52f1f58eea35b258c188f2f0cd526718
artemsalita <salita2212@gmail.com>
2018-12-26 17:06:06 +0600

    Testest2

---
+++ src/test/java/CupSuite.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class CupSuite {
+
+    @Test
+    public void testcup (){
+
+//       Cup
+    }
+}
---
+++ src/test/java/Testest2.java
@@ -0,0 +1,11 @@
+
+import static com.codeborne.selinide.Selenide.open;
+
+        public class Testest2 {
+
+        public void TestestTask ()  {
+
+        open ("")
+
+        }
+        }
