d6772c818491a6e7f22ed7de04aafb409cfae38d
artemsalita <salita2212@gmail.com>
2019-07-17 09:25:26 +0600

    17.07.19

--- src/test/java/PointSuite2.java
+++ src/test/java/PointSuite2.java
@@ -8,5 +8,11 @@ public class PointSuite2 {
         double result2;
        result2 = Point2.distanceBetween (1,1,3,4);
         Assert.assertEquals(3.6056, result2, 0.0001);
+        result2 = Point2.distanceBetween(-2, 4, 8, -10);
+        Assert.assertEquals(17.2046,result2,0.0001);
+        result2 = Point2.distanceBetween(0, 0, -5, -7);
+        Assert.assertEquals(8.6023,result2, 0.0001);
+        result2 = Point2.distanceBetween(-1, -3, 2, 9);
+        Assert.assertEquals(12.3693,result2, 0.0001);
     }
 }
