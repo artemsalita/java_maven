f12d067fa5f5b5de06274bd81e567ea4319d825c
artemsalita <salita2212@gmail.com>
2019-08-09 18:00:57 +0600

    09.08.19

--- src/main/java/zadachi/model_barbershop.java
+++ src/main/java/zadachi/model_barbershop.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class model_barbershop {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Random r = new Random();
 
@@ -12,57 +12,56 @@ public class model_barbershop {
         int startLunch = 204;
         int endLunch = 300;
         int endDay = 504;
-        int strtijka;
+        int strijka;
         int strijkaBrite;
 
         int tipClienta = r.nextInt(2);
 
-        if (tipClienta == 0){
-        strtijka = 12 + r.nextInt(24-12);
-            System.out.println("время стрижки" + strtijka + "минут");
-        }else {
-            strijkaBrite =  12 + r.nextInt(24-12)+ 8 + r.nextInt(12-8);
+        if (tipClienta == 0) {
+            strijka = 12 + r.nextInt(24 - 12);
+            System.out.println("время стрижки " + strijka + " минут");
+        } else {
+            strijkaBrite = 12 + r.nextInt(24 - 12) + 8 + r.nextInt(12 - 8);
 
-            System.out.println("время стрижки и бритья" + strijkaBrite + "минут");
+            System.out.println("время стрижки и бритья " + strijkaBrite + " минут");
         }
-
 
         System.out.println("");
 
-
         int prihodPervogoClienta = 25 + r.nextInt(45 - 25);
-        int prihodSledClienta = prihodPervogoClienta +25 + r.nextInt(45 - 25);
-
+        int prihodSledClienta = prihodPervogoClienta + 25 + r.nextInt(45 - 25);
 
         System.out.println("Первый клиент пришёл через " + prihodPervogoClienta + " минут");
         System.out.println("Следующий клиент пришёл через " + prihodSledClienta + " минут");
 
-
-            while (prihodSledClienta <= startLunch) {
-                prihodSledClienta = prihodSledClienta + 25 + r.nextInt(45 - 25);;
-
-                if (prihodSledClienta > startLunch){
-                   break;
-                }
-                System.out.println("Следующий клиент пришёл через " + prihodSledClienta + " минут");
-            }
-
-        System.out.println("Обед");
-
-            prihodSledClienta = endLunch;
-
-        while (prihodSledClienta <= endDay) {
-
+        while (prihodSledClienta <= startLunch) {
             prihodSledClienta = prihodSledClienta + 25 + r.nextInt(45 - 25);
 
-            if (prihodSledClienta > endDay){
+
+            if (prihodSledClienta > startLunch) {
                 break;
             }
             System.out.println("Следующий клиент пришёл через " + prihodSledClienta + " минут");
         }
 
+        System.out.println("");
+        System.out.println("Обед");
+        System.out.println("");
+
+        prihodSledClienta = endLunch;
+
+        while (prihodSledClienta <= endDay) {
+
+            prihodSledClienta = prihodSledClienta + 25 + r.nextInt(45 - 25);
+
+            if (prihodSledClienta > endDay) {
+                break;
+            }
+            System.out.println("Следующий клиент пришёл через " + prihodSledClienta + " минут");
         }
 
     }
+
+}
 
 
