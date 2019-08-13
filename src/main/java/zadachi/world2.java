3da518afed54fcceec1df009735218508d47f766
artemsalita <salita2212@gmail.com>
2019-08-13 20:15:03 +0600

    13/3.08.19

--- src/main/java/zadachi/world2.java
+++ src/main/java/zadachi/world2.java
@@ -10,8 +10,6 @@ public class world2 {
         int timer = 0;
         int tipClienta = r.nextInt(2);
         int endtime = -1;
-        int strijka;
-        int strijkaBrite;
         int prihodClienta = 25 + r.nextInt(45 - 25);
         int clients = -1;
         int startclients = 0;
@@ -26,39 +24,33 @@ public class world2 {
                 clients++;
                 System.out.println("Клиентов в очереди " + clients + " человек");
 
-                if (tipClienta == 0) {
-                    strijka = 12 + r.nextInt(24 - 12);
-                    System.out.println("Тип клиента 1" + " Время стрижки " + strijka + " минут");
-                    if (endtime <= prihodClienta){
-                        endtime = prihodClienta + strijka;
-                        System.out.println("Время простоя");
-                    } else {
-                        endtime = endtime + strijka ;
-                        System.out.println("Время ожидания");
-                    }
-                } else {
-                    strijkaBrite = 12 + r.nextInt(24 - 12) + 8 + r.nextInt(12 - 8);
-                    System.out.println("Тип клиента 2" + " Время стрижки и бритья " + strijkaBrite + " минут");
-
-                    if (endtime <= prihodClienta){
-                        endtime = prihodClienta + strijkaBrite;
-                        System.out.println("Время простоя");
-                    } else {
-                        endtime = endtime + strijkaBrite;
-                        System.out.println("Время ожидания");
-                    }
-                }
 
                 prihodClienta = prihodClienta + 25 + r.nextInt(45 - 25);
             }
             if (endtime == timer) {
+                int processDuration;
+                if (tipClienta == 0) {
+                    processDuration = 12 + r.nextInt(24 - 12);;
+                    System.out.println("Тип клиента 1" + " Время стрижки " + processDuration + " минут");
+                } else {
+                    processDuration = 12 + r.nextInt(24 - 12) + 8 + r.nextInt(12 - 8);;
+                    System.out.println("Тип клиента 2" + " Время стрижки и бритья " + processDuration + " минут");
+                }
+
+                if (endtime <= prihodClienta) {
+                    endtime = prihodClienta + processDuration;
+                    System.out.println("Время простоя");
+                } else {
+                    endtime = endtime + processDuration;
+                    System.out.println("Время ожидания");
+                }
                 System.out.println("Клиент обработан в " + endtime + " минут");
                 clients--;  //!! срабатывает только первый раз
                 System.out.println("Клиентов в очереди " + clients + " человек");
                 System.out.println("");
             }
 
-            if (timer >= endDay){
+            if (timer >= endDay) {
                 System.out.println("");
                 System.out.println("Рабочий день окончен");
                 break;
